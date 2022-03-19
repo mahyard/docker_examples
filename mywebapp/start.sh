711acd03cb98b4b8b5955ef17a7d89d3fe2292ac
Mahyar Damavand <mahyar.d.zig@gmail.com>
2022-03-19 11:38:11 +0330

    WIP: haproxy with reverse proxy

--- mywebapp/start.sh
+++ mywebapp/start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 envsubst '$MWA_DB_HOST,$MWA_DB_USER,$MWA_DB_PASS,$MWA_DB_NAME' < /var/www/html/config_sample.php > /var/www/html/config.php
+if [[ $ENALBE_REMOTE_IP ]]; then
+    REGEXP="s/<\/VirtualHost>/\n\tRemoteIPHeader X-Forwarded-For\n\tRemoteIPInternalProxy \/32\n<\/VirtualHost>/g"
+    sed -i "$REGEXP" /etc/apache2/sites-available/000-default.conf
+    a2enmod remoteip
+fi
 exec apache2-foreground
